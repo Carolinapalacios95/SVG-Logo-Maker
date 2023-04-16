64f4cc3a3e0f4abca3c6c269687046a1ee9008eb
Carolina <carolina.palacios1995@gmail.com>
2023-04-16 17:26:36 -0400

    added prompt questions for user input

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Please enter up to 3 characters for your logo.",
+        validate: textInput => {
+            if ((textInput) && (textInput <= 3)) {
+                return true;
+            } else {
+                console.log("Please enter up to 3 characters for your logo.")
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Please enter a text color keyword or hexadecimal number",
+        validate: textColorInput => {
+            if (textColorInput) {
+                return true;
+            } else {
+                console.log("Please enter a text color keyword or hexadecimal number");
+                return false;
+            }
+        }
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "Please select a shape from the following list.",
+        choices: ["circle", "triangle", "square"],
+        validate: shapeInput => {
+            if (shapeInput) {
+                return true;
+            } else {
+                console.log("Please select a shape from the following list.");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Please enter a shape color keyword or hexadecimal number",
+        validate: shapeColorInput => {
+            if (shapeColorInput) {
+                return true;
+            } else {
+                console.log("Please enter a shape color keyword or hexadecimal number");
+                return false;
+            }
+        }
+    }
+];
 
